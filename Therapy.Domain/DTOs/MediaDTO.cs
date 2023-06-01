0182a3316cb6afb21e4bab32a028271447488023
oscaralejandrofrancosilva <oskr.88@hotmail.com>
2023-06-01 16:39:34 -0600

    Accepting only Image or Video

--- Therapy.Domain/DTOs/MediaDTO.cs
+++ Therapy.Domain/DTOs/MediaDTO.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Therapy.Domain.DTOs {
   public class MediaDTO
   {
       public int Id { get; set; }
       public string Url { get; set; }
+      [RegularExpression("Image|Video")]
       public string Type { get; set; }
   }
 }
