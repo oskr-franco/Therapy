36363141b6b67aab58fb8072ea91664b1666be62
oscaralejandrofrancosilva <oskr.88@hotmail.com>
2023-07-01 13:51:14 -0600

    modify MediaUpdateDTO to id optional

--- Therapy.Domain/DTOs/MediaUpdateDTO.cs
+++ Therapy.Domain/DTOs/MediaUpdateDTO.cs
@@ -1,7 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Therapy.Domain.DTOs {
   /// <summary>
   /// AutoMapper is ignoring MediaDTO.Id, that is why we created this class to add another mapping(check MappingProfile).
   /// </summary>
-  public class MediaUpdateDTO: MediaDTO
-  { }
+  public class MediaUpdateDTO
+  { 
+    public int? Id { get; set; }
+    public string Url { get; set; }
+    [RegularExpression("Image|Video")]
+    public string Type { get; set; }
+  }
 }
