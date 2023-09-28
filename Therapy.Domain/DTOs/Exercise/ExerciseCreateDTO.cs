57079c852b5768da75c864012af868181e794ccf
oscaralejandrofrancosilva <oskr.88@hotmail.com>
2023-09-28 03:22:27 -0600

    making Instructions not required

--- Therapy.Domain/DTOs/Exercise/ExerciseCreateDTO.cs
+++ Therapy.Domain/DTOs/Exercise/ExerciseCreateDTO.cs
@@ -5,7 +5,7 @@ namespace Therapy.Domain.DTOs.Exercise {
     {
         public string Name { get; set; }
         public string Description { get; set; }
-        public string Instructions { get; set; }
+        public string? Instructions { get; set; }
         public List<MediaCreateDTO> Media { get; set; }
     }
 }
--- Therapy.Domain/DTOs/Exercise/ExerciseDTO.cs
+++ Therapy.Domain/DTOs/Exercise/ExerciseDTO.cs
@@ -6,7 +6,7 @@ namespace Therapy.Domain.DTOs.Exercise {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public string Instructions { get; set; }
+        public string? Instructions { get; set; }
         public List<MediaDTO> Media { get; set; }
     }
 }
